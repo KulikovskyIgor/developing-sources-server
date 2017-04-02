1b77dfb35f134ab61fc8f73023a8e5cc781f4a36
igor <kulikovsky93@gmail.com>
2017-04-02 22:07:48 +0300

    Initial commit

---
+++ src/api/article/model.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from 'mongoose'
+
+const articleSchema = new Schema({
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  title: {
+    type: String
+  },
+  content: {
+    type: String
+  }
+}, {
+  timestamps: true
+})
+
+articleSchema.methods = {
+  view (full) {
+    const view = {
+      // simple view
+      id: this.id,
+      user: this.user.view(full),
+      title: this.title,
+      content: this.content,
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt
+    }
+
+    return full ? {
+      ...view
+      // add properties for a full view
+    } : view
+  }
+}
+
+const model = mongoose.model('Article', articleSchema)
+
+export const schema = model.schema
+export default model
